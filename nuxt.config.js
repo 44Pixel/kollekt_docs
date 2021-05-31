49479013fedd3d2f9d12c38214900f41f15686a1
David-44Pixel <david@44pixel.com>
2021-05-31 10:13:44 +0200

    Deploy Kollekt Docs

---
+++ nuxt.config.js
@@ -0,0 +1,11 @@
+import theme from '@nuxt/content-theme-docs'
+
+export default theme({
+  docs: {
+    primaryColor: '#2a46ff'
+  },
+  target: 'static',
+  router: {
+    base: '/kollekt_docs/'
+  }
+})
